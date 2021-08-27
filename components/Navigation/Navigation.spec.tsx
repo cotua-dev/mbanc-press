f2f3194f1751520a60cb4faa54b68e100c5c8e93
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-27 15:08:07 -0700

    test(Navigation.spec.tsx): add tests for clicks

--- components/Navigation/Navigation.spec.tsx
+++ components/Navigation/Navigation.spec.tsx
@@ -1,15 +1,52 @@
-import { render, RenderResult } from '@testing-library/react';
+import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react';
 import { Navigation } from './index';
 
 describe('Navigation Component', (): void => {
     let renderResult: RenderResult;
 
-    beforeAll((): RenderResult => renderResult = render(<Navigation/>));
+    beforeEach((): RenderResult => renderResult = render(<Navigation/>));
 
     test('renders', (): void => {
         const { container } = renderResult;
         expect(container.firstChild).toMatchSnapshot();
     });
 
-    afterAll((): void => renderResult.unmount());
+    test('click dropdown activator', (): void => {
+        const { queryByTestId } = renderResult;
+
+        // Check to see if the dropdown body is not visible
+        expect(queryByTestId('dropdown-body')).toBeFalsy();
+
+        // Find and click on the dropdown activator
+        const dropdownActivator: HTMLElement = queryByTestId('dropdown-activator');
+        fireEvent.click(dropdownActivator);
+
+        // Check to see if the dropdown body is visible
+        const dropdownBody: HTMLElement = queryByTestId('dropdown-body');
+        expect(dropdownBody).toBeTruthy();
+    });
+
+    test('open and close mobile menu', (): void => {
+        const { queryByTestId } = renderResult;
+
+        // Set the innerWidth to mobile
+        global.innerWidth = 920;
+
+        // Click on the mobile menu button
+        const mobileMenuButton = queryByTestId('mobile-menu-button');
+        fireEvent.click(mobileMenuButton);
+
+        // Check to see if the mobile menu is truthy
+        const mobileMenu = queryByTestId('mobile-menu');
+        expect(mobileMenu).toBeTruthy();
+
+        // Click on the close mobile menu button
+        const closeMobileMenuButton = queryByTestId('close-mobile-menu-button');
+        fireEvent.click(closeMobileMenuButton);
+
+        // Reset innerWidth to desktop
+        global.innerWidth = 1080;
+    });
+
+    afterEach(cleanup);
 });
