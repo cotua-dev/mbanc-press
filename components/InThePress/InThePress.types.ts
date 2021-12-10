e8afd5ec07c1db7f6e8c71c58484d8e223ff453b
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-12-10 12:52:12 -0800

    chore(InThePress.types.ts): update `NewsOutlets`

--- components/InThePress/InThePress.types.ts
+++ components/InThePress/InThePress.types.ts
@@ -1,3 +1,3 @@
 export type NewsOutlets = 'USA Today' | 'The Sun' | 'Yahoo!' | 'The Washington Post' |
     'National Mortgage News' | 'Bankrate' | 'GoBankingRates' | 'MSN' | 'LendingTree' |
-    'Daily Nation Today';
+    'Daily Nation Today' | 'CNBC' | 'Business Insider';
