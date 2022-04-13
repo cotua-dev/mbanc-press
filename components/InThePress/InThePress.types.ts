b4a5da50f7cd4e1a27553b53df77db9277099f0b
Jeffrey Berry <jeffreyberry93@gmail.com>
2022-04-13 10:58:35 -0700

    chore(InThePress): add articles

--- components/InThePress/InThePress.types.ts
+++ components/InThePress/InThePress.types.ts
@@ -1,4 +1,5 @@
 export type NewsOutlets = 'USA Today' | 'The Sun' | 'Yahoo!' | 'The Washington Post' |
     'National Mortgage News' | 'Bankrate' | 'GoBankingRates' | 'MSN' | 'LendingTree' |
     'Daily Nation Today' | 'CNBC' | 'Business Insider' | 'NextAdvisor' | 'Forbes' |
-    'Money' | 'The Mortgage Reports' | 'MPA Mag' | 'Benzinga';
+    'Money' | 'The Mortgage Reports' | 'MPA Mag' | 'Benzinga' | 'Apartment Therapy' |
+    'US News';
